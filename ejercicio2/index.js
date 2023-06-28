eefaa6d2a91a14e70d00dd17d2f9bd6ef994d64e
kevinnncr <kacriollo@sudamericano.edu.ec>
2023-06-28 11:20:38 -0500

    tareas11

---
+++ ejercicio2/index.js
@@ -0,0 +1,15 @@
+var nombre = "kevin";
+var edad = 16;
+
+console.log("Mi nombre es", nombre, "y tengo", edad, "años.");
+
+if (edad >= 18) {
+  console.log("Soy mayor de edad.");
+} else {
+  console.log("Soy menor de edad.");
+}
+
+var estado = "soltero";
+if(estado = "soltero"){
+alert("Mi estado civil es "+ estado);
+}
